9c889ebdb03122eac33227bc45f40c8322232ed6
headEmpty <1393477335@qq.com>
2026-03-07 23:08:55 +0800

    H5打包配置调整

--- config/index.ts
+++ config/index.ts
@@ -109,6 +109,23 @@ export default defineConfig<'webpack5'>(async (merge, { command, mode }) => {
       },
       webpackChain(chain) {
         chain.resolve.plugin('tsconfig-paths').use(TsconfigPathsPlugin);
+        chain.optimization.splitChunks({
+          chunks: 'all',
+          cacheGroups: {
+            vendors: {
+              name: 'vendors',
+              test: /[\\/]node_modules[\\/]/,
+              priority: 10,
+              chunks: 'initial',
+            },
+            commons: {
+              name: 'commons',
+              minChunks: 2, // 被至少两个页面引用
+              priority: 5,
+              reuseExistingChunk: true,
+            },
+          },
+        });
       },
     },
     rn: {
